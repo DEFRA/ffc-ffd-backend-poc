0e5323249f6828538f4640f772a83823cad344f6
John Watson <john.watson1@defra.gov.uk>
2024-01-16 16:14:35 +0000

    consume message

--- app/index.js
+++ app/index.js
@@ -1,7 +1,22 @@
 require('./insights').setup()
+const { MessageReceiver } = require('ffc-messaging')
+
+const handleMessage = async (message) => {
+  console.log(message)
+}
 
 const init = async () => {
-  console.log('hello world')
+  const receiver = new MessageReceiver({
+    useCredentialChain: false,
+    host: process.env.MESSAGE_HOST,
+    username: process.env.MESSAGE_USER,
+    password: process.env.MESSAGE_PASSWORD,
+    address: 'ffc-ffd-backend-poc',
+    topic: 'ffc-ffd-data',
+    type: 'subscription'
+  }, handleMessage)
+
+  await receiver.subscribe()
 }
 
 process.on('unhandledRejection', (err) => {
